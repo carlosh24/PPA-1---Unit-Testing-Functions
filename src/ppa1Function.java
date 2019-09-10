26812ffa47d254326a8707123a4c8b07d0a9244f
Adam Wood <adamtylerwood0511@gmail.com>
2019-09-09 21:52:57 -0400

    add non decimal precision functionality to the shortestDistance function and make tests for the decimal precision that fail

--- src/ppa1Function.java
+++ src/ppa1Function.java
@@ -29,8 +29,7 @@ public class ppa1Function {
 	}
 	
 	public double shortestDistance(double x1, double y1, double x2, double y2) {
-		
-		return 0;
-	
+		double dist = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow(y2-y1, 2));
+		return dist;
 	}
 }
--- src/ppa1FunctionTest.java
+++ src/ppa1FunctionTest.java
@@ -6,6 +6,8 @@ class ppa1FunctionTest {
 	
 	ppa1Function test = new ppa1Function();
 	
+	/* TESTS FOR splitTheTip */
+	
 	@Test
 	public void evenTest() {
 		double []testResult1 = {5.75,0.0};	//expected value for test 1
@@ -31,10 +33,10 @@ class ppa1FunctionTest {
 	}
 	@Test
 	public void zeroTest() {
-		double []testResults = {-1, -1};
-		double []testResults2 = {0, 0};
-		assertArrayEquals(test.splitTheTip(15, 0), testResults, "it should return -1 so it shows that there is no guests. It prevents an error of dividing by 0");
-		assertArrayEquals(test.splitTheTip(0.0,2), testResults2, "0 + 0  tip divided by 2 people should be 0 per guest ");
+		double []testResult1 = {-1, -1};
+		double []testResult2 = {0, 0};
+		assertArrayEquals(test.splitTheTip(15, 0), testResult1, "it should return -1 so it shows that there is no guests. It prevents an error of dividing by 0");
+		assertArrayEquals(test.splitTheTip(0.0,2), testResult2, "0 + 0  tip divided by 2 people should be 0 per guest ");
 	}
 	@Test
 	public void typeTest() {
@@ -42,5 +44,23 @@ class ppa1FunctionTest {
 		t = test.splitTheTip(10, 2);
 		assertTrue(t instanceof double[]);
 		assertTrue(t.length==2);
+	}
+	
+	/* TESTS FOR shortestDistance */
+	
+	@Test
+	public void nonDecimalTest() {
+		double testResult1 = 1.0;
+		double testResult2 = 2.0;
+		assertEquals(test.shortestDistance(0.0, 0.0, 1.0, 0.0),testResult1,"The distance from the origin to 1,0 is 1");
+		assertEquals(test.shortestDistance(1.0, 1.0, 1.0, 3.0),testResult2,"The distance from 1,1 to 1,3 is 2");	
+	}
+	
+	@Test
+	public void decimalTest() {
+		double testResult1 = 2.7080;
+		double testResult2 = 60.9073;
+		assertEquals(test.shortestDistance(3.12, 4.67, 1.5, 2.5),testResult1,"The distance should be 2.7080 with 4 dp of precision");
+		assertEquals(test.shortestDistance(4.23, 5.14, 6.63, 66.0),testResult1,"The distance should be 60.9073 with 4 dp of precision");
 	}
 }
