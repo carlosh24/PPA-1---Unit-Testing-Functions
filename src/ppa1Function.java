e997fd6137b6715307877e2eec844e9cce659e57
carlos henriquez <carlos_247@live.com>
2019-09-09 20:13:18 -0400

    this is adding the round test

--- src/ppa1Function.java
+++ src/ppa1Function.java
@@ -4,6 +4,7 @@ public class ppa1Function {
 	public double[] splitTheTip(double dinnerAmmount, int guestNumb) {
 		double total = dinnerAmmount * 1.15;
 		double evenSplit = total / guestNumb;
+		evenSplit= Math.round(evenSplit * 100.0) / 100.0; //Rounds the total after tip to the nearest 2 decimal place.
 		double[] answer = {evenSplit, 0.0};
 		return answer;
 	}
--- src/ppa1FunctionTest.java
+++ src/ppa1FunctionTest.java
@@ -13,4 +13,12 @@ class ppa1FunctionTest {
 		assertArrayEquals(test.splitTheTip(12, 3),t2,"12 + 1.8 tip divided by 3 people should be 4.6 per guest");
 		
 	}
+	@Test
+	public void roundTest() {
+		ppa1Function test= new ppa1Function();
+		double []testResult1 = {6.60,0.0};
+		double []testResult2 = {4.62,0.0};
+		assertArrayEquals(test.splitTheTip(11.48, 2),testResult1,"11.48 + 1.722 tip divided by 2 people should be 6.6 per guest  when rounded");
+		assertArrayEquals(test.splitTheTip(12.05, 3),testResult2, "12.05 + 1.8075  tip divided by 2 people should be 4.62 per guest  when rounded");
+	}
 }
